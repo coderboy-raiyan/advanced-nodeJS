a5d8deeaf993c1473a051f5e0f97ddf32818efaf
Raiyan <tajkierhaque@gmail.com>
2022-11-05 19:44:05 +0600

    Basic form handeling example

---
+++ Basics/handeling-requests/handeling-requests.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      res.setHeader("Content-type", "text/html");
+      res.end(
+        `<form action="/message" method="POST"><input name="message"/> <button type="submit">send</button></form>`
+      );
+      return;
+
+    case "/message":
+      if (req.method === "POST") {
+        const body = [];
+        req.on("data", (chunk) => {
+          body.push(chunk);
+        });
+        req.on("end", () => {
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split("=")[1];
+          fs.writeFileSync("message.txt", message);
+        });
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+      }
+      return;
+
+    default:
+      break;
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Listening...");
+});
