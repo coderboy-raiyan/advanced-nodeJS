831aba1c66aaaf12e536e25177b3dddb3d5c9e43
Raiyan <tajkierhaque@gmail.com>
2022-11-28 12:26:44 +0600

    Post editing done

--- feed-app/server/src/routes/feed/feed.controller.js
+++ feed-app/server/src/routes/feed/feed.controller.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-use-before-define */
+const path = require('path');
+const fs = require('fs');
 const Post = require('../../models/Post.model');
 
 async function getPosts(req, res, next) {
@@ -14,8 +17,6 @@ async function getPosts(req, res, next) {
     }
 }
 async function createPost(req, res, next) {
-    console.log(req.file);
-
     if (!req.file) {
         return res.status(422).json({ message: 'No image provided' });
     }
@@ -59,4 +60,56 @@ async function getPost(req, res, next) {
     }
 }
 
-module.exports = { getPosts, createPost, getPost };
+async function updatePost(req, res, next) {
+    try {
+        const { postId } = req.params;
+        const { title, content } = req.body;
+
+        let imageUrl = req.body.image;
+
+        if (req.file) {
+            imageUrl = `images/${req.file.filename}`;
+        }
+
+        if (!imageUrl) {
+            return res.status(422).json({ message: 'No file picked' });
+        }
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({ message: 'Could not find post' });
+        }
+
+        if (imageUrl !== post.imgUrl) {
+            clearImage(post.imgUrl);
+        }
+
+        post.title = title;
+        post.content = content;
+        post.imgUrl = imageUrl;
+
+        const result = await post.save();
+
+        res.status(200).json({ message: 'Post Updated', post: result });
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+}
+
+function clearImage(filePath) {
+    if (filePath) {
+        filePath = path.join(__dirname, '..', '..', filePath);
+        fs.unlink(filePath, (err) => console.log(err));
+    }
+}
+
+module.exports = {
+    getPosts,
+    createPost,
+    getPost,
+    updatePost,
+};
--- feed-app/server/src/routes/feed/feed.router.js
+++ feed-app/server/src/routes/feed/feed.router.js
@@ -1,9 +1,9 @@
 const feedRouter = require('express').Router();
-const { getPosts, createPost, getPost } = require('./feed.controller');
+const { getPosts, createPost, getPost, updatePost } = require('./feed.controller');
 const uploader = require('../../lib/multer');
 
 feedRouter.route('/posts').get(getPosts);
 feedRouter.route('/post').post(uploader.single('image'), createPost);
-feedRouter.route('/post/:postId').get(getPost);
+feedRouter.route('/post/:postId').get(getPost).put(uploader.single('image'), updatePost);
 
 module.exports = feedRouter;
