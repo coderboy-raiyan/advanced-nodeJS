f0789651787591ad5887197a5cc88bf5f3ab7f9a
Raiyan <tajkierhaque@gmail.com>
2022-11-28 14:58:01 +0600

    client site cors added

--- feed-app/server/src/app/middlewares.js
+++ feed-app/server/src/app/middlewares.js
@@ -3,6 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-router.use([express.json(), morgan('dev'), cors({ origin: ['http://localhost:3000'] })]);
+router.use([
+    express.json(),
+    morgan('dev'),
+    cors({ origin: ['http://localhost:3000', process.env.FRONTEND_URL] }),
+]);
 
 module.exports = router;
